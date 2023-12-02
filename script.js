c58d226458b7988fa4693d996c7203725dd300ac
Devorae <demoraekaidavis@hotmail.co.uk>
2023-12-02 22:17:13 +0800

    added project page slider

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ function submitForm() {
         alert("Form submitted successfully!");
     }
 }
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    }
+});
